e039c6f23f7b2aa95bfc6edc7725c14ad868ae80
Yifan Hao <haoyifanzz312@gmail.com>
2026-01-02 23:45:24 -0500

    Bowling game 1

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,83 @@
+﻿namespace BowlingGame
+{
+    public class Game
+    {
+        //private int _itsScore;
+        private int[] _itsThrows;
+        private int _itsCurrentThrow;
+        private int _itsCurrentFrame;
+        private bool _firstThrow;
+        
+        public Game() 
+        {
+            //_itsScore = 0;
+            _itsThrows = new int[21];
+            _itsCurrentThrow = 0;
+            _itsCurrentFrame = 1;
+            _firstThrow = true;
+        }
+
+        
+
+        public int score()
+        {
+            return scoreForFrame(getCurrentFrame() - 1); 
+        }
+
+        public void add(int pins)
+        {
+            //_itsScore += pins;
+            _itsThrows[_itsCurrentThrow++] = pins;
+            adjustCurrentFrame(pins);
+        }
+
+        private void adjustCurrentFrame(int pins)
+        {
+            if (_firstThrow)
+            {
+                if (pins == 10)
+                    _itsCurrentFrame++;
+                else
+                    _firstThrow = false;
+            }
+            else
+            {
+                _firstThrow = true;
+                _itsCurrentFrame++;
+            }
+        }
+
+        public int scoreForFrame(int theFrame)
+        {
+            int score = 0;
+            int ball = 0;
+            for (int currentFrame = 0; 
+                currentFrame < theFrame; 
+                currentFrame++)
+            {
+                int firstThrow = _itsThrows[ball++];
+                if (firstThrow == 10)
+                {
+                    score += firstThrow + _itsThrows[ball] + _itsThrows[ball + 1];
+                }
+                else
+                {
+                    int secondThrow = _itsThrows[ball++];
+                    int framescore = firstThrow + secondThrow;
+
+                    if (framescore == 10)   //spare
+                        score += framescore + _itsThrows[ball];
+                    else
+                        score += framescore;
+                }
+            }
+
+            return score;
+        }
+
+        public int getCurrentFrame()
+        {
+            return _itsCurrentFrame;
+        }
+    }
+}
---
+++ TestBowlingGame/TestGame.cs
@@ -0,0 +1,86 @@
+﻿using BowlingGame;
+
+namespace TestBowlingGame
+{
+    public class TestGame
+    {
+        private Game g;
+
+        public TestGame()
+        {
+            g = new Game();
+        }
+
+
+        [Fact]
+        public void testScoreNoThrows()
+        {
+            Assert.Equal(0, g.score());
+        }
+
+        [Fact]
+        public void testAddOneThrowNoMark()
+        {
+            g.add(5);
+            g.add(4);
+            Assert.Equal(9, g.score());
+            Assert.Equal(2, g.getCurrentFrame());
+        }
+
+        [Fact]
+        public void testFourThrowsNoMark()
+        {
+            g.add(5);
+            g.add(4);
+            g.add(7);
+            g.add(2);
+            Assert.Equal(18, g.score());
+            Assert.Equal(9, g.scoreForFrame(1));
+            Assert.Equal(18, g.scoreForFrame(2));
+            Assert.Equal(3, g.getCurrentFrame());
+        }
+
+        [Fact]
+        public void testSimpleSpare()
+        {
+            g.add(3);
+            g.add(7);
+            g.add(3);
+            Assert.Equal(13, g.scoreForFrame(1));
+            Assert.Equal(2, g.getCurrentFrame());
+        }
+
+        [Fact]
+        public void testSimpleFrameAfterSpare()
+        {
+            g.add(3);
+            g.add(7);
+            g.add(3);
+            g.add(2);
+            Assert.Equal(13, g.scoreForFrame(1));
+            Assert.Equal(18, g.scoreForFrame(2));
+            Assert.Equal(18, g.score());
+            Assert.Equal(3, g.getCurrentFrame());
+        }
+
+        [Fact]
+        public void testTwoThrows()
+        {
+            g.add(5);
+            g.add(4);
+            Assert.Equal(9, g.score());
+            Assert.Equal(2, g.getCurrentFrame());
+        }
+
+        [Fact]
+        public void testSimpleStrike()
+        {
+            g.add(10);
+            g.add(3);
+            g.add(6);
+            Assert.Equal(19, g.scoreForFrame(1));
+            Assert.Equal(28, g.score());
+            Assert.Equal(3, g.getCurrentFrame());
+        }
+    }
+}
