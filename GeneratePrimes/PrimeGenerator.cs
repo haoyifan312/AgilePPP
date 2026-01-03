1fd628c42ae482ce2549a5eeb53fe87da637da5c
Yifan Hao <haoyifanzz312@gmail.com>
2026-01-02 21:49:52 -0500

    initial commit

---
+++ GeneratePrimes/PrimeGenerator.cs
@@ -0,0 +1,76 @@
+﻿namespace GeneratePrimes
+{
+    public class PrimeGenerator
+    {
+        private static bool[] crossedOut;
+
+        public static int[] generatePrimes(int maxValue)
+        {
+            if (maxValue < 2)         
+                return new int[0];
+            else
+            {
+                uncrossIntegersUoTo(maxValue);
+                crossOutMultiples();
+                return putUncrossedIntegersIntoResult();
+            }
+
+        }
+
+        private static void uncrossIntegersUoTo(int maxValue)
+        {
+            crossedOut = new bool[maxValue + 1];
+            for (int i = 2; i < crossedOut.Length; i++)
+                crossedOut[i] = false;
+        }
+
+        private static void crossOutMultiples()
+        {
+            int j;
+            int maxPrimeFactor = calcMaxPrimeFactor();
+            for (int i = 2; i < maxPrimeFactor; i++)
+            {
+                if (!crossedOut[i])   //if i is uncrossed, cross its multiples
+                    crossOutMultiplesOf(i);
+            }
+        }
+
+        private static void crossOutMultiplesOf(int i)
+        {
+            for (int multiple = 2 * i; multiple < crossedOut.Length; multiple += i)
+                crossedOut[multiple] = true;   //multiple is not prime
+        }
+
+        private static int calcMaxPrimeFactor()
+        {
+            double maxPrimeFactor = Math.Sqrt(crossedOut.Length) + 1;
+            return (int)maxPrimeFactor;
+        }
+
+        private static int[] putUncrossedIntegersIntoResult()
+        {
+            int[] primes = new int[numberOfUncrossedIntegers()];
+
+            // move the primes into the result
+            for (int i = 2, j = 0; i < crossedOut.Length; i++)
+            {
+                if (!crossedOut[i])
+                    primes[j++] = i;
+            }
+
+            return primes;
+        }
+
+        private static int numberOfUncrossedIntegers()
+        {
+            // how many primes are there?
+            int count = 0;
+            for (int i = 2; i < crossedOut.Length; i++)
+            {
+                if (!crossedOut[i])
+                    count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ TestPrimeGenerator/TestPrimeGenerator.cs
@@ -0,0 +1,46 @@
+﻿using GeneratePrimes;
+
+namespace TestPrimeGenerator
+{
+    public class TestGeneratePrimes
+    {
+        [Fact]
+        public void testPrimes()
+        {
+            int[] nullArray = GeneratePrimes.PrimeGenerator.generatePrimes(0);
+            Assert.Empty(nullArray);
+
+            int[] minArray = GeneratePrimes.PrimeGenerator.generatePrimes(2);
+            Assert.Single(minArray);
+            Assert.Equal(2, minArray[0]);
+
+            int[] threeArray = GeneratePrimes.PrimeGenerator.generatePrimes(3);
+            Assert.Equal(2, threeArray.Length);
+            Assert.Equal(2, threeArray[0]);
+            Assert.Equal(3, threeArray[1]);
+
+            int[] centArray = GeneratePrimes.PrimeGenerator.generatePrimes(100);
+            Assert.Equal(25, centArray.Length);
+            Assert.Equal(97, centArray[24]);
+        }
+
+        [Fact]
+        public void testExhaustive()
+        {
+            for (int i = 2; i < 500; i++)
+                verifyPrimeList(GeneratePrimes.PrimeGenerator.generatePrimes(i));
+        }
+
+        private void verifyPrimeList(int[] list)
+        {
+            for (int i = 0; i < list.Length; i++)
+                verifyPrime(list[i]);
+        }
+
+        private void verifyPrime(int n)
+        {
+            for (int factor = 2; factor < n; factor++)
+                Assert.True(n % factor != 0);
+        }
+    }
+}
